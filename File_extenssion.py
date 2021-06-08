0d28cd57d692d063c1ee86b04e2b3cab3d03ed22
rdb9 <83170562+rdb9@users.noreply.github.com>
2021-06-08 21:59:04 +0530

    touch to view

---
+++ File_extenssion.py
@@ -0,0 +1,4 @@
+#program to find extension of file
+filename = input("Input the Filename: ")    #name of the file
+f_extns = filename.split(".")
+print ("The extension of the file is : ",f_extns[-1])    #file Extension
